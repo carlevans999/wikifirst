1c03959d5555329fb1303dbb184e18a4b1378f00
wfq <wangfangqing@ebupt.com>
2023-09-05 22:25:07 +0800

    新增test.http

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,14 +1,23 @@
 package com.example.wiki.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController //返回字符串
 //@Controller //返回页面，前后端分离用不到
 public class TestController {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
-        return "Hello";
+        return "Hello get ";
+    }
+
+    @PostMapping("/hello")
+    public String hellopost(String name) {
+        return "Hello post." + name;
     }
 }
